4ed5a51ecdfbb4986b3c2862d864d714fda8b486
João Paulo Canário <jopacanario@gmail.com>
2014-01-30 19:22:01 -0300

    Removing unnecessary white space from code

--- avltree/main.c
+++ avltree/main.c
@@ -6,32 +6,22 @@
 #include "avltree.h"
 
 int main(int argc, const char * argv[]) {
-    
     int vector[] = {1, 3, 5, 7, 8, 9, 10, 13, 14, 17};
-    
     int size = sizeof(vector) / sizeof(*vector);
     
     avl_node *root = NULL;
     
     for (int i = 0; i < size; i++) {
-        
         printf("\nInserting in tree value: %d\n", vector[i]);
         root = insert(root, vector[i]);
     }
     
     printf("\n------ Printing tree ------\n\n");
-    
     inorder(root);
     
     printf("\n\n------ Height of tree ------\n");
-    
     int height = find_height(root);
-    
-    printf("\nThe height of tree is: %d", height);
-    
-    printf("\n\n");
-    
-    
+    printf("\nThe height of tree is: %d\n\n", height);
     
     return 1;
 }
